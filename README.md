03b3769648f5788efd893c4c655bca4c052eb275
carlosmonsivais123 <carlos.s.monsivais@gmail.com>
2022-07-08 01:20:33 -0700

    new readme, data extraction class and main file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PySpark Stock Classification Project
+The goal is to optimize a portfolio to the N number of publically traded companies that give the mot returns using
+PySpark big data methods and create data pipleines within the Google Cloud Platform.
---
+++ main.py
@@ -0,0 +1,6 @@
+from Retrieve_Stock_Data.ticker_data import Get_Stock_Data
+
+# Extracting and Creating Dataset
+get_stock_data = Get_Stock_Data() # Class name Get_Stock_Data()
+get_stock_data.sp_500_ticks() # Extracting S&P 500 ticker symbols
+get_stock_data.yahoo_finance_data() # Extracting all S&P 500 data from 2017-01-01 to 2022-06-07
