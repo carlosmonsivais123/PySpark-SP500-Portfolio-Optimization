88f0853a8c7734bd3d4d2cfffa5363c708f38281
carlosmonsivais123 <carlos.s.monsivais@gmail.com>
2022-07-18 20:26:30 -0700

    notebook to python files

---
+++ PySpark_Data/data_schema.py
@@ -0,0 +1,16 @@
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType, TimestampType
+
+class Original_Schema:
+    def full_stock_data_schema():
+        stock_schema = StructType([StructField('Symbol', StringType(), False),
+                                   StructField('Date', TimestampType(), False),
+                                   StructField('Open', FloatType(), True),
+                                   StructField('High', FloatType(), True),
+                                   StructField('Low', FloatType(), True),
+                                   StructField('Close', FloatType(), True),
+                                   StructField('Adj Close', FloatType(), True),
+                                   StructField('Volume', IntegerType(), True),
+                                   StructField('Description', StringType(), False),
+                                   StructField('Category2', StringType(), False),
+                                   StructField('Category3', StringType(), False),
+                                   StructField('GICS Sector', StringType(), False)])
