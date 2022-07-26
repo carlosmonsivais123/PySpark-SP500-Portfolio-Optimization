2fe33c8b34cbdba371aa990fc314412a69b07ff6
carlosmonsivais123 <carlos.s.monsivais@gmail.com>
2022-07-25 21:16:22 -0700

    new testing on the eda plots only now

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from data_cleaning import Data_Cleaning_Stock
+from stock_plots import EDA_Plots
 
 # Data Cleaning: data_cleaning.py
-data_cleaning_stock = Data_Cleaning_Stock()
-data_cleaning_stock.read_in_data_data_cleaning()
-data_cleaning_stock.null_value_analysis()
+# data_cleaning_stock = Data_Cleaning_Stock()
+# data_cleaning_stock.read_in_data_data_cleaning()
+# data_cleaning_stock.null_value_analysis()
 
 # EDA: stock_plots.py
+eda_plots = EDA_Plots()
+eda_plots.read_in_data_data_cleaning()
