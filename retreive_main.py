59e4e44f1ee47506730e0b32ca5dfbe5d84553bc
carlosmonsivais123 <carlos.s.monsivais@gmail.com>
2022-08-02 23:31:47 -0700

    Moved all actions to the cloud only

--- retreive_main.py
+++
@@ -1,7 +0,0 @@
-import sys
-from Retrieve_Stock_Data.retreive_data import Get_Stock_Data
-
-# Extracting and Creating Dataset
-get_stock_data = Get_Stock_Data() # Class name Get_Stock_Data()
-get_stock_data.sp_500_ticks() # Extracting S&P 500 ticker symbols
-get_stock_data.yahoo_scrape() # Extracting all S&P 500 data
