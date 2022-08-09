903b4bc07ab751cc7f6de0f54601f8dcc6d7ecf0
carlosmonsivais123 <carlos.s.monsivais@gmail.com>
2022-08-09 01:02:33 -0700

    added 5 different model types

--- PySpark_ML_Models/linear_regression_models.py
+++ PySpark_ML_Models/linear_regression_models.py
@@ -7,9 +7,14 @@ from pyspark.sql.types import StructType, StructField, StringType, FloatType, Ti
 from pyspark.sql.functions import pandas_udf, PandasUDFType
 
 import pandas as pd
+import numpy as np
 import joblib
 from google.cloud import storage
 from sklearn.linear_model import LinearRegression
+from sklearn.neural_network import MLPRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.compose import ColumnTransformer
@@ -24,20 +29,21 @@ class ML_Model:
 
 
     def linear_regression_models(self):
-        vars_needed = self.stock_df_clean.select('Symbol', 'Date', 'lag_1', 'day_of_week', 'month', 'volume_lag_1', 'daily_return')
+        vars_needed = self.stock_df_clean.select('Symbol', 'Date', 'lag_1', 'lag_2', 'lag_3', 'lag_4', 'lag_5', 'lag_6', 
+                                                 'day_of_week', 'month', 'volume_lag_1', 'daily_return')
         
         # PandasUDF Inputs
         group_column = 'Symbol'
         y_column = 'daily_return'
-        x_columns = ['lag_1', 'day_of_week', 'month', 'volume_lag_1']
+        x_columns = ['lag_1', 'lag_2', 'lag_3', 'lag_4', 'lag_5', 'lag_6', 'day_of_week', 'month', 'volume_lag_1']
         random_state = 10
 
         modeling_schema = StructType([StructField('date', TimestampType(), True),
-                                     StructField('symbol', StringType(), True),
-                                     StructField('daily_return', FloatType(), True),
-                                     StructField('pred_daily_return', FloatType(), True),
-                                     StructField('rmse', FloatType(), True)]) 
-
+                                    StructField('symbol', StringType(), True),
+                                    StructField('model_type', StringType(), True),
+                                    StructField('daily_return', FloatType(), True),
+                                    StructField('pred_daily_return', FloatType(), True),
+                                    StructField('rmse', FloatType(), True)]) 
 
 
         @pandas_udf(modeling_schema, PandasUDFType.GROUPED_MAP)
@@ -46,29 +52,49 @@ class ML_Model:
             group_key = pdf[group_column].iloc[0]
             
             ohe_transform = ColumnTransformer(transformers=[('onehot', OneHotEncoder(), ['day_of_week', 'month'])], 
-                                            remainder='passthrough')
+                                              remainder='passthrough')
             
             X = ohe_transform.fit_transform(pdf[x_columns])    
             y = pdf[y_column]
 
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=10, shuffle=False)
+            X_train, X_test, y_train, y_test = train_test_split(X, 
+                                                                y, 
+                                                                test_size=0.20, 
+                                                                random_state=random_state, 
+                                                                shuffle=False)
             
-            reg = LinearRegression().fit(X_train, y_train)
+            model_algs = {'LR': LinearRegression(), 
+                          'KNN': KNeighborsRegressor(), 
+                          'RF': RandomForestRegressor(random_state = random_state),
+                          'GB': GradientBoostingRegressor(random_state=random_state),
+                          'NN': MLPRegressor(random_state=random_state)}
+            rmse_values = []
+            
+            for key, value in model_algs.items():
+                reg = value.fit(X_train, y_train)
+                y_preds = value.predict(X_test)
+                rmse = mean_squared_error(y_test, y_preds, squared=False)
+                rmse_values.append(rmse)
+                
+            best_model_index = np.argmin(rmse_values)
+            model_type = list(model_algs.values())[best_model_index]
+            model_name = list(model_algs.keys())[best_model_index]
+            
+            reg = model_type.fit(X_train, y_train)
             y_preds = reg.predict(X_test)
             rmse = mean_squared_error(y_test, y_preds, squared=False)
 
             dates = pdf.iloc[y_test.index]['Date']
-            
-            preds_df = pd.DataFrame({'date': dates,
-                                     'symbol': group_key, 
-                                     'daily_return': y_test, 
-                                     'pred_daily_return': y_preds,
-                                     'rmse': rmse})
-            
-            # Export the model to a file
-            model_filename = f'{group_key}_lr_model.joblib'
+        
+            pred_df = pd.DataFrame({'date': dates,
+                                    'symbol': group_key,
+                                    'model_type': model_name,
+                                    'daily_return': y_test, 
+                                    'pred_daily_return': y_preds,
+                                    'rmse': rmse})
+
+            model_filename = f'{group_key}_{model_name}_model.joblib'
             joblib.dump(reg, model_filename)
-            
             
             bucket_name="stock-sp500"
             destination_blob_name=f"Modeling/Models/{model_filename}"
@@ -79,8 +105,9 @@ class ML_Model:
 
             blob.upload_from_filename(model_filename)
             
-            
-            return preds_df
+            return pred_df
+
+
         
         lr_stocks = vars_needed.groupby("Symbol").apply(linear_regression).toPandas()
         lr_stocks.to_csv("gs://stock-sp500/Modeling/predictions.csv", index = False, header = True)
